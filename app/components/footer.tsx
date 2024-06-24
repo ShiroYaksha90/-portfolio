7a95e6b4b7abe89d912ee53455135f7da98ceaf1
ShiroYaksha90 <aljazaeribasim@gmail.com>
2024-06-24 23:43:59 +0300

    feat: Add Footer component for website

---
+++ app/components/footer.tsx
@@ -0,0 +1,37 @@
+import Link from "next/link";
+
+type FooterLinkProps = {
+  text: string;
+  url: string;
+};
+
+function FooterLink(props: FooterLinkProps) {
+  let { text, url } = props;
+  return (
+    <Link className="transtion hover:text-tal-500" href={url}>
+      {text}
+    </Link>
+  );
+}
+export default function Footer() {
+  return (
+    <>
+      <footer className="pt-10 px-8 pb-16 border-t">
+        <div
+          className="flex justify-between gap-6"
+          role="navigation"
+          aria-label="Footer"
+        >
+          <div className="flex gap-6 text-sm font-medium text-zinc-600">
+            <FooterLink text="Home" url="/" />
+            <FooterLink text="Projects" url="/projects" />
+            <FooterLink text="About" url="/about" />
+          </div>
+          <p className="text-sm text-zinc-400">
+            &copy; 2024 Basim Aljazaeri. All rights reserved.
+          </p>
+        </div>
+      </footer>
+    </>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar";
+import Footer from "./components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,8 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <link rel="icon" href="/favicon.ico" />
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
